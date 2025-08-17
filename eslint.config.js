fa917579a04d403c3ab588fc495765c2f3fe3a7a
Jon Nadelberg <jvnadelberg@gmail.com>
2025-08-16 18:52:03 -0700

    chore(eslint): flat config with Node globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 const js = require("@eslint/js");
+const globals = require("globals");
 
 module.exports = [
+  { languageOptions: { globals: { ...globals.node } } },
   js.configs.recommended,
-  {
-    ignores: ["node_modules/**","dist/**","data/**"]
-  }
+  { ignores: ["node_modules/**","dist/**","data/**"] }
 ];
